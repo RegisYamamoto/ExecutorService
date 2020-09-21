cc9f466120d8258a018e5ef7533640e9fe5e0758
regisyamamoto <regisyamamoto@gmail.com>
2020-09-21 19:49:32 -0300

    Ajuste

--- src/com/regis/executorservice/service/PessoaService.java
+++ src/com/regis/executorservice/service/PessoaService.java
@@ -4,29 +4,38 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class PessoaService {
 
 	public void criarListas() {
-		System.out.println("---------- Iniciou o programa ----------");
+		try {
+			System.out.println("---------- Iniciou o programa ----------");
+			
+			List<String> nomes1 = Arrays.asList("Pedro", "Douglas");
+			List<String> nomes2 = Arrays.asList("Ana", "Gabriel");
+			List<String> nomes3 = Arrays.asList("Valéria", "Paulo");
+			
+			
+			// Montar e excutar as threads
+			ExecutorService executor = Executors.newFixedThreadPool(3);
+			
+			Runnable r1 = () -> imprimirLista(nomes1);
+			Runnable r2 = () -> imprimirLista(nomes2);
+			Runnable r3 = () -> imprimirLista(nomes3);
+			
+			executor.execute(r1);
+			executor.execute(r2);
+			executor.execute(r3);
 		
-		List<String> nomes1 = Arrays.asList("Pedro", "Douglas");
-		List<String> nomes2 = Arrays.asList("Ana", "Gabriel");
-		List<String> nomes3 = Arrays.asList("Valéria", "Paulo");
-		
-		
-		// Montar e excutar as threads
-		ExecutorService executorService = Executors.newFixedThreadPool(3);
-		
-		Runnable r1 = () -> imprimirLista(nomes1);
-		Runnable r2 = () -> imprimirLista(nomes2);
-		Runnable r3 = () -> imprimirLista(nomes3);
-		
-		executorService.execute(r1);
-		executorService.execute(r2);
-		executorService.execute(r3);
+			executor.shutdown();
+			executor.awaitTermination(Long.MAX_VALUE, TimeUnit.HOURS);
 
-		System.out.println("---------- Terminou o programa ----------");
+			System.out.println("---------- Terminou o programa ----------");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public void imprimirLista(List<String> nomes) {
